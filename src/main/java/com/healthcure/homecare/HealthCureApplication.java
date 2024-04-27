68fdf5104dfa9a5ace5644bee6feb80f56c171b2
Soumo <csoumo@gmail.com>
2024-04-27 17:05:21 +0530

    push changes

---
+++ src/main/java/com/healthcure/homecare/HealthCureApplication.java
@@ -0,0 +1,15 @@
+package com.healthcure.homecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class HealthCureApplication {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthCureApplication.class, args);
+    }
+
+
+}
